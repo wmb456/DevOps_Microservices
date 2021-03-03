d9539eb1d5710cc98b5a8197fc946dc2362e3948
Thomas Tannhaeuser (FG-752LZ) <Thomas.Tannhaeuser@bmw.de>
2021-03-03 23:36:47 +0100

    update upload_docker script

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
+. config.sh
+
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+DOCKERPATH=${HUB_USER}/${IMAGE_NAME}
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: $DOCKERPATH"
+docker login --username=${HUB_USER}
+docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${DOCKERPATH}:${IMAGE_TAG}
 
 # Step 3:
 # Push image to a docker repository
+docker push ${DOCKERPATH}
+
+
+
