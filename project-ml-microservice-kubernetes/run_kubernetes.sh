966e4efe71f69d166729ed39c8e4c1fd18a0e2a1
Ubuntu <ubuntu@ip-172-31-7-191.eu-central-1.compute.internal>
2021-03-04 21:57:09 +0000

    run container with cubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -3,16 +3,19 @@
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ttannhaeuser/udacity-app:0.0.1
 
 # Step 2
-# Run the Docker Hub container with kubernetes
-
+kubectl run udacityapp \
+	    --generator=run-pod/v1 \
+	    --image=$dockerpath \
+	    --port=80 --labels app=udacityapp
 
 # Step 3:
-# List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
-# Forward the container port to a host
+kubectl port-forward udacityapp 8000:80
+
 
